d917ecd3f5e6283e85ff6f25dd153ce8b9296f2c
Andrew Trofimchuk <zipppt@gmail.com>
2015-11-05 00:05:56 +0200

    sort

---
+++ booble sort.py
@@ -0,0 +1,15 @@
+l = [32, 9, 1, 184, 5, 4]
+
+i = 0
+temp = None
+while i < len(l) - 1:
+    j = 0
+    while j < len(l) - 1:
+        if l[j] > l[j + 1]:
+            temp = l[j]
+            l[j] = l[j + 1]
+            l[j + 1] = temp
+        j = j + 1
+    i = i + 1
+
+print(l)
